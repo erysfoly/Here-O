ebd1b401fb29bd512dd41e5fe141fa25aac8c6cb
erysfoly <erysfoly@gmail.com>
2022-02-19 00:13:01 +0100

    edit migration for postgresql

--- migrations/Version20220207203121.php
+++ migrations/Version20220207203121.php
@@ -20,7 +20,7 @@ final class Version20220207203121 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE quest (id SERIAL, title VARCHAR(100) NOT NULL, author VARCHAR(50) NOT NULL, description VARCHAR(1000) NOT NULL, date DATETIME NOT NULL, place VARCHAR(255) NOT NULL, people_number INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE quest (id SERIAL PRIMARY KEY, title VARCHAR(100) NOT NULL, author VARCHAR(50) NOT NULL, description VARCHAR(1000) NOT NULL, date DATETIME NOT NULL, place VARCHAR(255) NOT NULL, people_number INT NOT NULL) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
