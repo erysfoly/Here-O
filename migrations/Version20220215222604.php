548aacd25e963da027b5721d09de7129417d2149
erysfoly <erysfoly@gmail.com>
2022-02-19 00:28:34 +0100

    edit migration for postgresql

--- migrations/Version20220215222604.php
+++ migrations/Version20220215222604.php
@@ -20,12 +20,12 @@ final class Version20220215222604 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE quest ADD picture VARCHAR(255) DEFAULT \'https://cdn.pixabay.com/photo/2018/12/14/11/55/volunteers-3874924_960_720.png\' NOT NULL, DROP image');
+        $this->addSql('ALTER TABLE quest ADD picture VARCHAR(255) DEFAULT \'https://cdn.pixabay.com/photo/2018/12/14/11/55/volunteers-3874924_960_720.png\' NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE quest ADD image VARCHAR(255) DEFAULT \'https://cdn.pixabay.com/photo/2018/12/14/11/55/volunteers-3874924_960_720.png\' NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP picture, CHANGE title title VARCHAR(100) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE author author VARCHAR(50) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description VARCHAR(1000) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE place place VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE quest DROP picture, CHANGE title title VARCHAR(100) NOT NULL, CHANGE author author VARCHAR(50) NOT NULL, CHANGE description description VARCHAR(1000) NOT NULL, CHANGE place place VARCHAR(255) NOT NULL');
     }
 }
